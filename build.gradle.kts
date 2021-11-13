e7d4fbfd8dc96899712a3859097023c0cb1d1876
A-Imal <Abdullah.Imal@hivemq.com>
2021-11-13 15:23:57 +0100

    Migrate from Maven to Gradle.

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("com.hivemq.extension")
+    id("com.github.hierynomus.license")
+    id("com.github.sgtsilvio.gradle.utf8")
+    id("org.asciidoctor.jvm.convert")
+}
+
+group = "com.bytechaos.mqtt"
+description = "Enable MQTT 5 features for MQTT 3 Clients."
+
+hivemqExtension {
+    name.set("MQTT 5 for 3")
+    author.set("Byte Chaos")
+    priority.set(1000)
+    startPriority.set(10000)
+    sdkVersion.set("${property("hivemq-extension-sdk.version")}")
+}
+
+/* ******************** resources ******************** */
+
+val prepareAsciidoc by tasks.registering(Sync::class) {
+    from("README.adoc").into({ temporaryDir })
+}
+
+tasks.asciidoctor {
+    dependsOn(prepareAsciidoc)
+    sourceDir(prepareAsciidoc.map { it.destinationDir })
+}
+
+hivemqExtension.resources {
+    from("LICENSE")
+    from("README.adoc") { rename { "README.txt" } }
+    from(tasks.asciidoctor)
+}
+
+/* ******************** test ******************** */
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${property("junit-jupiter.version")}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
+/* ******************** integration test ******************** */
+
+dependencies {
+    integrationTestImplementation("org.testcontainers:testcontainers:${property("testcontainers.version")}")
+    integrationTestImplementation("com.hivemq:hivemq-testcontainer-junit5:${property("hivemq-testcontainer.version")}")
+    integrationTestRuntimeOnly("ch.qos.logback:logback-classic:${property("logback.version")}")
+}
+
+/* ******************** checks ******************** */
+
+license {
+    header = rootDir.resolve("HEADER")
+    mapping("java", "SLASHSTAR_STYLE")
+}
